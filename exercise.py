b5f0dd8b4283ebafe47ab02edfe2569fccc362eb
Rizzuto82 <135610413+Rizzuto82@users.noreply.github.com>
2023-08-25 20:12:15 +0200

    Add files via upload

--- exercise.py
+++ exercise.py
@@ -1,3 +1,17 @@
-import os
-x = os.getlogin()
-print(x)
+class Animal:
+    def __init__(self, animal, leg):
+        self.animal = animal
+        self.leg = leg
+    def runs(self):    
+
+     Animal1 = Animal("dog", 4)
+     Animal1.runs()
+print ("Animal1 object was created")
+print ("Running started")
+       
+
+
+
+
+    
+

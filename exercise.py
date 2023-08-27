58ff0b32b000b87bd9f07e2bce70c1f1fb68a7c1
Rizzuto82 <135610413+Rizzuto82@users.noreply.github.com>
2023-08-27 19:32:42 +0200

    Add files via upload

--- exercise.py
+++ exercise.py
@@ -1,32 +1,20 @@
-# Gaetano so già che è sbagliato ma mi confondo troppo
-
 class Animal:
-    def __init__(self,legs_count):
-        print("Animal object was created")
-        self._number_of_legs = legs_count
+    def __init__(self, animal, count_leg):
+        self.animal = animal
+        self.count_leg = count_leg
+        print("animal object was created")
 
     def runs(self):
-        print("Running started")
+        print("Running started")    
 
-    def count_legs(self):
-        print(f"It has {self._number_of_legs}")
+animal1 = Animal("dog", 4)       
+animal1.runs() 
 
-    def return_legs(self):
-        return self._number_of_legs
-        
-animal= Animal()
-animal.count_legs()
-print(animal.return_legs())
-print(animal._number_of_legs)
 
-class Dog (Animal):
-    def __init__(self, name, legs_count):
-        Animal.__init__(self, name, legs_count)
-        
-    def bark(self):
-        print("woof woof")
+       
 
-dog = Dog = ("Bob", 4)
 
-dog.bark()
-print(dog.legs_count)
+
+
+    
+
